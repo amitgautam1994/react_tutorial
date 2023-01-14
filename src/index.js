cad19595bbeb5e84f8c392b344d927b74c0c4bb7
amitgautam1 <amitgautam@edugorilla.org>
2023-01-14 19:20:17 +0530

    removed testing folder

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import ReactDom from "react-dom";
 import './index.css'
 import {books} from './books'
 import Book from "./Book";
-import { greeting } from "./testing/testing";
 
 function BookList(){
-  console.log(greeting)
   return (
     <section className="booklist">
       {books.map((book) => {
